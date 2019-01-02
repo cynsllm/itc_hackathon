c6886d88b8d948cc2ac5b280356709e3b120e649
Axel <axelpriven@gmail.com>
2019-01-02 19:42:19 +0200

    more

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,44 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=tel%20aviv&APPID=13f1f80994cd71ca577de534eddb82af&units=metric%22", function( data ) {
-   console.log(data);
-   var city = data.name;
-   console.log(city)
-  var temp = data.main.temp;
-  var humid = data.main.humidity;
-  console.log("the temperature is " + temp)
-  console.log("the humidity is " + humid)
-  $(".weather").text("In " + city + " the temp is " + temp + " and the humidity is " + humid + "%")
-   });
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=tel%20aviv&APPID=13f1f80994cd71ca577de534eddb82af&units=metric%22", function (data) {
+    console.log(data);
+    var city = data.name;
+    console.log(city)
+    var temp = data.main.temp;
+    var humid = data.main.humidity;
+    console.log("the temperature is " + temp)
+    console.log("the humidity is " + humid)
+    $(".weather").text("In " + city + " the temp is " + temp + " and the humidity is " + humid + "%")
+});
 
 
-  $(document).ready(function () {
-        $("#myBtn").click(function(){
-            if ($(".chat-box.shadowed").css("display") === "none") {
-                $(".chat-box.shadowed").css("display", "block");
-            } else {
-                $(".chat-box.shadowed").css("display", "none")
-            }
-        });
-  });
+$(document).ready(function () {
+    $("#myBtn").click(function () {
+        if ($(".chat-box.shadowed").css("display") === "none") {
+            $(".chat-box.shadowed").css("display", "block");
+        } else {
+            $(".chat-box.shadowed").css("display", "none")
+        }
+    });
+    $(".cows").on("click", function () {
+        $("#info_cows").css("display", "block");
+    });
+    $(".close").on("click", function () {
+        $("#info_cows").css("display", "none");
+    });
+    $(".milk").on("click", function () {
+        $("#info_milk").css("display", "block");
+    });
+    $(".close").on("click", function () {
+        $("#info_milk").css("display", "none");
+    });
+    $(".healthy").on("click", function () {
+        $("#info_healthy").css("display", "block");
+    });
+    $(".close").on("click", function () {
+        $("#info_healthy").css("display", "none");
+    });
+});
+
+
 
 
 
